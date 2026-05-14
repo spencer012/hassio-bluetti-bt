544f991ac69cd06eece2258a4200ff6c9801af0a
Spencer <43939691+spencer012@users.noreply.github.com>
2026-05-13 23:18:31 -0500

    fix encrypted notification handling

--- custom_components/bluetti_bt/coordinator.py
+++ custom_components/bluetti_bt/coordinator.py
@@ -8,9 +8,71 @@ from homeassistant.components import bluetooth
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from bluetti_bt_lib import build_device, DeviceReader, DeviceReaderConfig
+from bluetti_bt_lib.bluetooth.encryption import Message, MessageType
+from bluetti_bt_lib.const import WRITE_UUID
 
 from .utils import mac_loggable
 from .types import FullDeviceConfig
+
+
+class SafeDeviceReader(DeviceReader):
+    """Device reader that ignores malformed encrypted notifications."""
+
+    async def _notification_handler(self, _: int, data: bytearray):
+        """Handle bt data."""
+        self.logger.debug("Got new data")
+
+        if self.config.use_encryption is True:
+            message = Message(data)
+
+            if message.is_pre_key_exchange:
+                message.verify_checksum()
+
+                if message.type == MessageType.CHALLENGE:
+                    challenge_response = self.encryption.msg_challenge(message)
+                    await self.client.write_gatt_char(WRITE_UUID, challenge_response)
+                    return
+
+                if message.type == MessageType.CHALLENGE_ACCEPTED:
+                    self.logger.debug("Challenge accepted")
+                    return
+
+            if self.encryption.unsecure_aes_key is None:
+                self.logger.warning(
+                    "Received encrypted message before key initialization"
+                )
+                return
+
+            key, iv = self.encryption.getKeyIv()
+            try:
+                decrypted = Message(self.encryption.aes_decrypt(message.buffer, key, iv))
+            except ValueError as err:
+                self.logger.debug("Ignoring invalid encrypted notification: %s", err)
+                return
+
+            if decrypted.is_pre_key_exchange:
+                decrypted.verify_checksum()
+
+                if decrypted.type == MessageType.PEER_PUBKEY:
+                    peer_pubkey_response = self.encryption.msg_peer_pubkey(decrypted)
+                    await self.client.write_gatt_char(WRITE_UUID, peer_pubkey_response)
+                    return
+
+                if decrypted.type == MessageType.PUBKEY_ACCEPTED:
+                    self.encryption.msg_key_accepted(decrypted)
+                    return
+
+            data = decrypted.buffer
+
+        self.notify_response.extend(data)
+
+        if self.notify_future is None:
+            return
+
+        if self.notify_future.done():
+            return
+
+        self.notify_future.set_result(self.notify_response)
 
 
 class PollingCoordinator(DataUpdateCoordinator):
@@ -43,7 +105,7 @@ class PollingCoordinator(DataUpdateCoordinator):
             self.async_shutdown()
             return None
 
-        self.reader = DeviceReader(
+        self.reader = SafeDeviceReader(
             config.address,
             bluetti_device,
             self.hass.loop.create_future,
